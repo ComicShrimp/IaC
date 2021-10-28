13c7610c6f65b13f91156d5f7c2016b174cb2ff2
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2021-10-28 10:59:15 -0300

    chore: add terraform-101 example

--- variables.tf
+++ variables.tf
@@ -1,28 +1,9 @@
-variable "region" {
-  description = "Define the region in wich region the instance will be deployed"
-  default     = "us-east-1"
-}
-
-variable "name" {
-  description = "Name of the application"
-  default     = "server01"
-}
-variable "env" {
-  description = "Environment of the application"
-  default     = "dev"
-}
-
-variable "instance_type" {
-  description = "AWS instance type"
-  default     = "t2.micro"
-}
-
-variable "repo" {
-  description = "Repository of the application"
-  default     = "github.com/ComicShrimp/IaC"
-}
-
 variable "vpc_cidr_block" {
   description = "Range of IPv4 address for the VPC"
   default     = "172.17.0.0/16"
+}
+
+variable "region" {
+  description = "Define the region in wich region the instance will be deployed"
+  default     = "us-east-1"
 }
